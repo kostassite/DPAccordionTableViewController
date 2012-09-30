a5b5b8f5d3991074fb4ca01aa572367e13a770fc
Kostas Antonopoulos <kostas@devpro.gr>
2012-09-30 17:22:44 +0300

    Initial Commit

---
+++ DPAccordionTableView/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  DPAccordionTableView
+//
+//  Created by Kostas Antonopoulos on 9/30/12.
+//  Copyright (c) 2012 Kostas Antonopoulos. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ DPAccordionTableView/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  DPAccordionTableView
+//
+//  Created by Kostas Antonopoulos on 9/30/12.
+//  Copyright (c) 2012 Kostas Antonopoulos. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
